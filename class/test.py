c9de0c3b7d3749fdbc7efebf19b1478a9fcbe6f7
JohPel <johannes@pelda.de>
2017-01-12 11:39:36 +0100

    import project

---
+++ class/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  7 22:44:05 2017
+
+@author: johannes
+"""
+
+class test():
+    def __init__(self, i):
+        nix=0
+        self.__i = i
+
+    def was(self):
+        pass
